21ba7d788e5569a4fcebab4c4856f50326426731
Chuck Luciano <chuck@scalability-labs.com>
2022-01-20 23:35:49 +0000

    Update instructions

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// These functions are to remain unchanged
 func getByte(r io.Reader) (b byte, err error) {
 	buf := make([]byte, 1)
 	_, err = r.Read(buf)
@@ -26,6 +27,21 @@ func check(err error) {
 	}
 }
 
+// This program copies from inputStream to outputStream by reading and writing
+// bytes through the provided interface.
+//
+// A new requirement has come up:
+//    Any place that "elvis" appears in the stream, it must be replaced with "Elvis"
+//    Everything else must be passed through unchanged.
+//    The input stream may be arbitrarily long and free of delimiters, so the solution
+//    must operate on streams.
+//
+// Please do not use any library functions unless excepted below:
+//   bytes.Compare:
+//     func Compare(a, b []byte) int
+//
+// The assigment will involve modifying/replacing the loop from line 61 to 67
+
 func main() {
 	var err error
 
@@ -34,14 +50,20 @@ func main() {
 
 	// inputStream := os.Stdin
 
-	outputStream := os.Stdout
-	var b byte
-	b, err = getByte(inputStream) // returns io.EOF at end of stream
+	searchBytes := []byte{'e', 'l', 'v', 'i', 's'}
+	replaceBytes := []byte{'E', 'l', 'v', 'i', 's'}
+	_ = searchBytes
+	_ = replaceBytes
 
+	outputStream := os.Stdout
+
+	var b byte
 	for err == nil {
+		b, err = getByte(inputStream) // returns io.EOF at end of stream
+		if err != nil {
+			break
+		}
 		err = putByte(outputStream, b)
-		check(err)
-		b, err = getByte(inputStream)
 	}
 	if err == io.EOF {
 		fmt.Println("SUCCESS")
